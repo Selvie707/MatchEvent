86e0ec737a46689434d11ae5ac51627b76458c28
serviawafa <serviawafa84@student.ub.ac.id>
2023-12-05 00:45:55 +0700

    XML Onboarding, SignUp, Login

---
+++ app/src/main/java/com/example/eventmatchmaker/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventmatchmaker.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventmatchmaker.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/eventmatchmaker/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventmatchmaker.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventmatchmaker.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
